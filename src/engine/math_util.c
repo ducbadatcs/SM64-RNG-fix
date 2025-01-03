eb0400042d48c372766f5a7873831638c66e1b85
Hoàng Minh Đức <71325886+ducbadatcs@users.noreply.github.com>
2025-01-03 16:36:51 +0700

    changed that

--- src/engine/math_util.c
+++ src/engine/math_util.c
@@ -25,30 +25,14 @@ Vec3s gVec3sZero = {     0,     0,     0 };
 Vec3i gVec3iZero = {     0,     0,     0 };
 Vec3s gVec3sOne  = {     1,     1,     1 };
 
-static u16 gRandomSeed16;
+static u16 gRandomSeed16 = 1;
 
 // Generate a pseudorandom integer from 0 to 65535 from the random seed, and update the seed.
 u16 random_u16(void) {
-    if (gRandomSeed16 == 22026) {
-        gRandomSeed16 = 0;
-    }
-
-    u16 temp1 = (((gRandomSeed16 & 0x00FF) << 8) ^ gRandomSeed16);
-
-    gRandomSeed16 = ((temp1 & 0x00FF) << 8) + ((temp1 & 0xFF00) >> 8);
-
-    temp1 = (((temp1 & 0x00FF) << 1) ^ gRandomSeed16);
-    u16 temp2 = ((temp1 >> 1) ^ 0xFF80);
-
-    if ((temp1 & 0x1) == 0) {
-        if (temp2 == 43605) {
-            gRandomSeed16 = 0;
-        } else {
-            gRandomSeed16 = (temp2 ^ 0x1FF4);
-        }
-    } else {
-        gRandomSeed16 = (temp2 ^ 0x8180);
-    }
+    // use the xorshift algorithm for a 32-bit int, then take the last 16 digits
+    gRandomSeed16 ^= (gRandomSeed16 << 7);
+    gRandomSeed16 ^= (gRandomSeed16 >> 9);
+    gRandomSeed16 ^= (gRandomSeed16 << 8);
 
     return gRandomSeed16;
 }
@@ -62,6 +46,7 @@ f32 random_float(void) {
 s32 random_sign(void) {
     return ((random_u16() >= 0x7FFF) ? 1 : -1);
 }
+
 
 // Get the maximum and minimum of three numbers at the same time.
 #define min_max_3_func(a, b, c, min, max) { \
